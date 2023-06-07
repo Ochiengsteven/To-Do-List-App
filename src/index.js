a340f5a30a76da83fe5f1b10b0448feb26521e13
Ochiengsteven <stevenochieng6305@gmail.com>
2023-06-07 15:36:09 +0300

    add styles and icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
+import reload from './assets/reload.png';
+import move from './assets/vertical-dots.png';
 
 const tasks = [
   {
@@ -18,18 +20,40 @@ const tasks = [
   },
 ];
 
+const reloadIcon = new Image();
+reloadIcon.src = reload;
+
+const moveIcon = new Image();
+moveIcon.src = move;
+
 const renderTasks = () => {
   const todoList = document.getElementById('todo-list');
   const todoContainer = document.createElement('div');
+  const taskTitle = document.createElement('li');
+  taskTitle.classList.add('task-title');
+  // eslint-disable-next-line quotes
+  taskTitle.innerHTML = `<h5>Today's To Do</h5>`;
+  taskTitle.append(reloadIcon);
+  const addTask = document.createElement('li');
+  addTask.classList.add('add-task');
+  addTask.innerHTML = '<input type="text" name="todo" placeholder="Add to your list..." />';
+  const clearTask = document.createElement('li');
+  clearTask.classList.add('clear-task');
+  clearTask.innerHTML = '<a href="#" onclick="">Clear all completed</a>';
   todoContainer.classList.add('list-container');
   todoList.innerHTML = '';
+  todoContainer.appendChild(taskTitle);
+  todoContainer.appendChild(addTask);
 
   tasks.forEach((task) => {
-    const list = document.createElement('ul');
-    list.innerHTML = `<li><label for="todo"><input type="checkbox"  name="todo" placeholder="Enter tasks" />${task.description}</label></li>`;
+    const list = document.createElement('li');
+    list.classList.add('task');
+    list.innerHTML = `
+    <label for="todo"><input type="checkbox"  name="todo" placeholder="Enter tasks" />${task.description}</label><img src="${moveIcon.src}" alt="Move">`;
     todoContainer.appendChild(list);
     todoList.appendChild(todoContainer);
   });
+  todoContainer.append(clearTask);
 };
 
 document.addEventListener('DOMContentLoaded', renderTasks);
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -19,6 +20,10 @@ module.exports = {
       {
         test: /\.css$/, // for all css files
         use: ['style-loader', 'css-loader'], // use these loaders
+      },
+      {
+        test: /\.(?:ico|png|svg|jpg|jpeg|gif)$/i, // for all images
+        type: 'asset/resource', // use asset/resource to emit the file as-is and import it
       },
     ],
   },
