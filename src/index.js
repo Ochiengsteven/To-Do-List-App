f5b6571e8d533f0b895a81ef3aefcb46202c4d23
Ochiengsteven <stevenochieng6305@gmail.com>
2023-06-09 10:11:56 +0300

    add add-task function

--- src/index.js
+++ src/index.js
@@ -1,59 +1,63 @@
+// import css stylesheet
 import './style.css';
+// import icon images
 import reload from './assets/reload.png';
 import move from './assets/vertical-dots.png';
+import add from './assets/plus.png';
+// import modules
 
-const tasks = [
-  {
-    description: 'task 1',
-    done: false,
-    id: 1,
-  },
-  {
-    description: 'task 2',
-    done: true,
-    id: 1,
-  },
-  {
-    description: 'task 3',
-    done: false,
-    id: 1,
-  },
-];
-
+// icons
 const reloadIcon = new Image();
 reloadIcon.src = reload;
 
 const moveIcon = new Image();
 moveIcon.src = move;
 
-const renderTasks = () => {
-  const todoList = document.getElementById('todo-list');
-  const todoContainer = document.createElement('div');
-  const taskTitle = document.createElement('li');
-  taskTitle.classList.add('task-title');
-  // eslint-disable-next-line quotes
-  taskTitle.innerHTML = `<h5>Today's To Do</h5>`;
-  taskTitle.append(reloadIcon);
-  const addTask = document.createElement('li');
-  addTask.classList.add('add-task');
-  addTask.innerHTML = '<input type="text" name="todo" placeholder="Add to your list..." />';
-  const clearTask = document.createElement('li');
-  clearTask.classList.add('clear-task');
-  clearTask.innerHTML = '<a href="#" onclick="">Clear all completed</a>';
-  todoContainer.classList.add('list-container');
-  todoList.innerHTML = '';
-  todoContainer.appendChild(taskTitle);
-  todoContainer.appendChild(addTask);
+const addIcon = new Image();
+addIcon.src = add;
 
-  tasks.forEach((task) => {
-    const list = document.createElement('li');
-    list.classList.add('task');
-    list.innerHTML = `
-    <label for="todo"><input type="checkbox"  name="todo" placeholder="Enter tasks" />${task.description}</label><img src="${moveIcon.src}" alt="Move">`;
-    todoContainer.appendChild(list);
-    todoList.appendChild(todoContainer);
+// Create the to do list HTML structure
+const todoContainer = document.getElementById('todo-list');
+const todoContent = document.createElement('div');
+todoContent.classList.add('task-content');
+const todoTitle = document.createElement('h2');
+todoTitle.classList.add('task-title');
+todoTitle.textContent = 'Today\'s To Do';
+const todoInput = document.createElement('div');
+todoInput.classList.add('task-input');
+todoInput.innerHTML = '<input type="text" id="input-to-do" placeholder="Add to your list..." />';
+const addButton = document.createElement('button');
+addButton.classList.add('add-button');
+addButton.innerHTML = '';
+addButton.appendChild(addIcon);
+todoInput.appendChild(addButton);
+todoTitle.appendChild(reloadIcon);
+todoContent.appendChild(todoTitle);
+todoContent.appendChild(todoInput);
+todoContent.innerHTML += '<ul id="list-container"><!-- <li class="checked">task1</li> --></ul>';
+todoContainer.appendChild(todoContent);
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Create the to do list functionality
+  const inputBox = document.getElementById('input-to-do');
+  const inputListContainer = document.getElementById('list-container');
+  const addButton = document.querySelector('.add-button');
+  const errorMessage = document.createElement('p');
+  errorMessage.classList.add('error-message');
+  errorMessage.style.display = 'none';
+  // eslint-disable-next-line no-undef
+  todoContent.appendChild(errorMessage);
+
+  addButton.addEventListener('click', () => {
+    if (inputBox.value === '') {
+      errorMessage.textContent = 'Please enter a task!';
+      errorMessage.style.display = 'block';
+    } else {
+      const li = document.createElement('li');
+      li.textContent = inputBox.value;
+      inputListContainer.appendChild(li);
+      inputBox.value = ''; // Clear the input field after adding the task
+      errorMessage.style.display = 'none'; // Hide the error message
+    }
   });
-  todoContainer.append(clearTask);
-};
-
-document.addEventListener('DOMContentLoaded', renderTasks);
+});
