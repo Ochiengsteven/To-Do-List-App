b743cf09063da29ec82433d1e4ac6b80ea57786e
Ochiengsteven <stevenochieng6305@gmail.com>
2023-06-09 13:08:02 +0300

    save files in local stoage as arrays

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-use-before-define */
 // import css stylesheet
 import './style.css';
 // import icon images
 import reload from './assets/reload.png';
 import move from './assets/vertical-dots.png';
 import add from './assets/plus.png';
-// import modules
 
 // icons
 const reloadIcon = new Image();
@@ -16,34 +16,49 @@ moveIcon.src = move;
 const addIcon = new Image();
 addIcon.src = add;
 
-// Create the to do list HTML structure
+// Create the to-do list HTML structure
 const todoContainer = document.getElementById('todo-list');
 const todoContent = document.createElement('div');
 todoContent.classList.add('task-content');
 const todoTitle = document.createElement('h2');
 todoTitle.classList.add('task-title');
-todoTitle.textContent = 'Today\'s To Do';
+todoTitle.textContent = "Today's To Do";
 const todoInput = document.createElement('div');
 todoInput.classList.add('task-input');
 todoInput.innerHTML = '<input type="text" id="input-to-do" placeholder="Add to your list..." />';
 const addButton = document.createElement('button');
 addButton.classList.add('add-button');
 addButton.innerHTML = '';
+const clearAll = document.createElement('p');
+clearAll.classList.add('clear-all');
+clearAll.textContent = 'Clear All';
 addButton.appendChild(addIcon);
 todoInput.appendChild(addButton);
 todoTitle.appendChild(reloadIcon);
 todoContent.appendChild(todoTitle);
 todoContent.appendChild(todoInput);
 todoContent.innerHTML += '<ul id="list-container"><!-- <li class="checked">task1</li> --></ul>';
+todoContent.appendChild(clearAll);
 todoContainer.appendChild(todoContent);
 
 document.addEventListener('DOMContentLoaded', () => {
-  // save data to local storage
-  const savedata = () => {
-    // eslint-disable-next-line no-use-before-define
-    localStorage.setItem('data', inputListContainer.innerHTML);
+  // save and get data to local storage
+  const saveData = () => {
+    localStorage.setItem('data', JSON.stringify(tasks));
   };
-  // Create the to do list functionality
+
+  const getData = () => {
+    const data = localStorage.getItem('data');
+    if (data) {
+      tasks = JSON.parse(data);
+      renderTasks();
+    }
+  };
+
+  // To-do list data
+  let tasks = [];
+
+  // Create the to-do list functionality
   const todoContent = document.querySelector('.task-content');
   const inputBox = document.getElementById('input-to-do');
   const inputListContainer = document.getElementById('list-container');
@@ -51,32 +66,83 @@ document.addEventListener('DOMContentLoaded', () => {
   const errorMessage = document.createElement('p');
   errorMessage.classList.add('error-message');
   errorMessage.style.display = 'none';
-  // eslint-disable-next-line no-undef
   todoContent.appendChild(errorMessage);
 
-  addButton.addEventListener('click', () => {
+  // Function to render tasks on the list
+  const renderTasks = () => {
+    inputListContainer.innerHTML = '';
+    tasks.forEach((task, index) => {
+      const li = document.createElement('li');
+      li.textContent = task.description;
+      if (task.completed) {
+        li.classList.add('checked');
+      }
+      const deleteButton = document.createElement('span');
+      deleteButton.innerHTML = '\u2716';
+      deleteButton.addEventListener('click', () => {
+        deleteTask(index);
+      });
+      li.appendChild(deleteButton);
+      inputListContainer.appendChild(li);
+    });
+    saveData();
+  };
+
+  // Function to add a new task
+  const addTask = () => {
     if (inputBox.value === '') {
       errorMessage.textContent = 'Please enter a task!';
       errorMessage.style.display = 'block';
     } else {
-      const li = document.createElement('li');
-      li.textContent = inputBox.value;
-      inputListContainer.appendChild(li);
-      inputBox.value = ''; // Clear the input field after adding the task
-      errorMessage.style.display = 'none'; // Hide the error message
-      const span = document.createElement('span');
-      span.innerHTML = '\u2716';
-      li.appendChild(span);
+      const newTask = {
+        description: inputBox.value,
+        completed: false,
+        index: tasks.length + 1,
+      };
+      tasks.push(newTask);
+      inputBox.value = '';
+      errorMessage.style.display = 'none';
+      renderTasks();
     }
-    savedata();
+  };
+
+  // Function to delete a task
+  const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    tasks.forEach((task, i) => {
+      task.index = i + 1;
+    });
+    renderTasks();
+  };
+
+  // Function to edit a task description
+  // eslint-disable-next-line no-unused-vars
+  const editTaskDescription = (index, newDescription) => {
+    tasks[index].description = newDescription;
+    renderTasks();
+  };
+
+  addButton.addEventListener('click', addTask);
+  inputBox.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      addTask();
+    }
   });
 
   inputListContainer.addEventListener('click', (e) => {
     if (e.target.tagName === 'LI') {
-      e.target.classList.toggle('checked');
-    } else if (e.target.tagName === 'SPAN') {
-      const div = e.target.parentNode;
-      div.remove();
+      const index = Array.from(inputListContainer.children).indexOf(e.target);
+      tasks[index].completed = !tasks[index].completed;
+      renderTasks();
     }
-  }, false);
+  });
+
+  const clearAllTasks = () => {
+    tasks = [];
+    renderTasks();
+  };
+
+  clearAll.addEventListener('click', clearAllTasks);
+
+  getData();
 });
